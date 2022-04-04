47414ec5e0b506863248dcd9ffd0b0589905c725
Cal <calnite@aestrolos.noreply.github.com>
2022-04-04 11:26:08 +0200

    animation

---
+++ sprint2/scrolltrigger.js
@@ -0,0 +1,51 @@
+function scrollTrigger(selector, options = {}) {
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+      addObserver(el, options)
+    })
+  }
+  function addObserver(el, options) {
+    // Check if `IntersectionObserver` is supported
+    if(!('IntersectionObserver' in window)) {
+      // Simple fallback
+      // The animation/callback will be called immediately so
+      // the scroll animation doesn't happen on unsupported browsers
+      if(options.cb){
+        options.cb(el)
+      } else{
+        entry.target.classList.add('active')
+      }
+      return
+    }
+    let observer = new IntersectionObserver((entries, observer) =>; {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          if(options.cb) {
+            options.cb(el)
+          } else{
+            entry.target.classList.add('active')
+          }
+          observer.unobserve(entry.target)
+        }
+      })
+    }, options)
+    observer.observe(el)
+  }
+  
+  // scrollTrigger('#rap-text')
+  scrollTrigger('.char', {
+    rootMargin: '-100vh',
+  })
+
+  /*
+  scrollTrigger('.loader', {
+    rootMargin: '-200px',
+    cb: function(el){
+      el.innerText = 'Loading...'
+      setTimeout(() => {
+        el.innerText = 'Task Complete!'
+      }, 1000)
+    }
+  })
+  */
